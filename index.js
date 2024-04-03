6144f1f5d352db9af4bb826c53d8f48a30cfcd45
Christian Lisangola <jochri3@gmail.com>
2024-04-03 20:26:05 +0200

    NPM intro

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-const http = require("node:http");
+// import http from "node:http";
 
-const tasks = [
-  { id: 1, taskName: "Create home page", completed: false },
-  { id: 2, taskName: "Create about page", completed: true },
-];
-const server = http.createServer((req, res) => {
-  if (req.method === "GET" && req.url === "/api/tasks") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(tasks));
-  }
-});
+// const tasks = [
+//   { id: 1, taskName: "Create home page", completed: false },
+//   { id: 2, taskName: "Create about page", completed: true },
+// ];
+// const server = http.createServer((req, res) => {
+//   if (req.method === "GET" && req.url === "/api/tasks") {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(JSON.stringify(tasks));
+//   }
+// });
 
-const port = 3000;
+// const port = 3000;
 
-server.listen(port, () => {
-  console.log(`Le serveur écoute sur le port ${port}`);
-});
+// server.listen(port, () => {
+//   console.log(`Le serveur écoute sur le port ${port}`);
+// });
+import axios from "axios";
+
+axios
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then((res) => console.log(res.data))
+  .catch((err) => console.log(err.message));
