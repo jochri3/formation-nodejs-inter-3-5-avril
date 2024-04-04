68791d14e18cb652c72258076d3ba2ef63a2e04b
Christian Lisangola <jochri3@gmail.com>
2024-04-04 10:03:27 +0200

    npm

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-// import http from "node:http";
-
-// const tasks = [
-//   { id: 1, taskName: "Create home page", completed: false },
-//   { id: 2, taskName: "Create about page", completed: true },
-// ];
-// const server = http.createServer((req, res) => {
-//   if (req.method === "GET" && req.url === "/api/tasks") {
-//     res.writeHead(200, { "Content-Type": "application/json" });
-//     res.end(JSON.stringify(tasks));
-//   }
-// });
-
-// const port = 3000;
-
-// server.listen(port, () => {
-//   console.log(`Le serveur écoute sur le port ${port}`);
-// });
-import axios from "axios";
+const axios = require("axios");
+const _ = require("lodash");
 
 axios
   .get("https://jsonplaceholder.typicode.com/users")
   .then((res) => console.log(res.data))
   .catch((err) => console.log(err.message));
+
+console.log(_.includes([1, 2, 3, 4, 5], 2));
